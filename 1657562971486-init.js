fb38f30f2d6c10db7646dac0df63c822d45f9c24
romanxeo <romanxeo@gmail.com>
2022-07-11 22:11:03 +0300

    first commit

---
+++ 1657562971486-init.js
@@ -0,0 +1,11 @@
+const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class init1657562971486 {
+
+    async up(queryRunner) {
+    }
+
+    async down(queryRunner) {
+    }
+
+}
